35d2237eab6c036e3a8e7a084ea195d16faf981a
Bikash Sharma <bikash1999@yahoo.com>
2018-05-22 13:18:10 +0530

    added docker for spark kafka

---
+++ twitter-pipe/app/stream.py
@@ -0,0 +1,2 @@
+
+print("hello from spark docker")
--- twitter-stream/app.py
+++ twitter-stream/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import tweepy
 
 app = Flask(__name__)
 
 @app.route("/")
 def send():
-	return "Hello how is it going!!!"
+	return "Hello how is it going...kjskfljsdrwerwer!!!"
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=80)
