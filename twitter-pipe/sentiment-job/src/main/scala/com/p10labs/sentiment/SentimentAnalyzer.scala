f804c2daf74f62559ef08c21913f3813f41f35a6
Shruti Tanwar <shruti.tanwar93@gmail.com>
2018-05-30 08:34:38 +0530

    added env variables

--- twitter-pipe/sentiment-job/src/main/scala/com/p10labs/sentiment/SentimentAnalyzer.scala
+++ twitter-pipe/sentiment-job/src/main/scala/com/p10labs/sentiment/SentimentAnalyzer.scala
@@ -1,18 +1,21 @@
 package com.p10labs.sentiment
 
 import org.apache.spark.SparkConf
-import org.apache.spark.serializer.KryoSerializer
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming.{Durations, StreamingContext}
+import org.apache.spark.serializer.KryoSerializer
+
 import org.apache.spark.streaming.kafka010._
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.kafka.common.serialization.StringDeserializer
 
-//import org.apache.spark.SparkContext
-//import org.apache.spark.rdd.RDD
-//import org.apache.spark.sql.SaveMode
-//import org.apache.kafka.clients.consumer.ConsumerRecord
-//import org.p7h.spark.sentiment.corenlp.CoreNLPSentimentAnalyzer
-//import org.p7h.spark.sentiment.mllib.MLlibSentimentAnalyzer
+/*import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SaveMode
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.p7h.spark.sentiment.corenlp.CoreNLPSentimentAnalyzer
+import org.p7h.spark.sentiment.mllib.MLlibSentimentAnalyzer*/
 
 object SentimentAnalyzer {
 
@@ -22,10 +25,13 @@ object SentimentAnalyzer {
   def main(args: Array[String]) {
 
     val ssc = StreamingContext.getActiveOrCreate(createSparkStreamingContext)
+
     val kafkaParams = Map[String, Object](
-      "bootstrap.servers" -> kafkaHost,
+      "bootstrap.servers" -> "localhost:9092",
       "group.id" -> "sentiment",
       "auto.offset.reset" -> "latest",
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
       "enable.auto.commit" -> (false: java.lang.Boolean)
     )
     val topics = Array("hashtag")
@@ -34,30 +40,33 @@ object SentimentAnalyzer {
       PreferConsistent,
       Subscribe[String, String](topics, kafkaParams)
     )
-    print(stream)
-    stream.map(record => (record.key, record.value))
+    //print(stream)
+    //stream.map(record => (record.key, record.value))
 
     stream.foreachRDD { rdd =>
-      if (rdd != null && !rdd.isEmpty() && !rdd.partitions.isEmpty) {
+      print("rdd"+"\n")
+      print(rdd)
+      if (rdd != null && !rdd.isEmpty()){ //&& !rdd.partitions.isEmpty) {
+        print("@@@@@@@@@@@")
         rdd.collect().foreach(println)
       }
     }
-    //ssc.stop
+    ssc.start()
+    ssc.awaitTermination()
   }
 
-  /**
-    * Create StreamingContext.
-    * @return StreamingContext
-    */
+  /* Create StreamingContext.
+   * @return StreamingContext
+   */
   def createSparkStreamingContext(): StreamingContext = {
     val conf = new SparkConf()
-      .setAppName("sentiment-analyzer")
+      .setAppName("sentiment-job")
       // Use KryoSerializer for serializing objects as JavaSerializer is too slow.
       .set("spark.serializer", classOf[KryoSerializer].getCanonicalName)
       // Reduce the RDD memory usage of Spark and improving GC behavior.
       .set("spark.streaming.unpersist", "true")
-      //.setMaster("local")
-      .setMaster(sparkMaster)
+      .setMaster("local")
+      //.setMaster(sparkMaster)
     val ssc = new StreamingContext(conf, Durations.seconds(15))
     ssc
   }
