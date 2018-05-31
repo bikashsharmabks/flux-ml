c215c85874416a4d2755b502f2713d4b9e2a4fa3
Bikash Sharma <bikash1999@yahoo.com>
2018-05-31 15:36:19 +0530

    bootstrapped react ui

---
+++ twitter-ui/src/containers/Full/Full.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Header from '../../components/Header/';
+import Sidebar from '../../components/Sidebar/';
+import Aside from '../../components/Aside/';
+import Footer from '../../components/Footer/';
+
+import Breadcrumbs from 'react-breadcrumbs';
+
+class Full extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Header />
+        <div className="app-body">
+          <main className="main">
+            <div className="container-fluid">
+              {this.props.children}
+            </div>
+          </main>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Full;
