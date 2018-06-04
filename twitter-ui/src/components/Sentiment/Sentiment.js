eddde0350926053ac33dca24dcf6b8a909454922
Bikash Sharma <bikash1999@yahoo.com>
2018-06-04 12:09:21 +0530

    added sentiment chart

--- twitter-ui/src/components/Sentiment/Sentiment.js
+++ twitter-ui/src/components/Sentiment/Sentiment.js
@@ -1,4 +1,26 @@
 import React, { Component } from 'react';
+import { Doughnut } from 'react-chartjs-2';
+
+const doughnut = {
+  labels: [
+    'Negative',
+    'Postive',
+    'Netural'
+  ],
+  datasets: [{
+    data: [300, 50, 100],
+    backgroundColor: [
+    '#FF6384',
+    '#4dbd74',
+    '#FFCE56'
+    ],
+    hoverBackgroundColor: [
+    '#FF6384',
+    '#4dbd74',
+    '#FFCE56'
+    ]
+  }]
+};
 
 class Sentiment extends Component {
 
@@ -14,6 +36,12 @@ class Sentiment extends Component {
                 <i className="fa fa-smile-o text-success"></i>
             </div>
             <div className="h4 mb-0">Sentiment Analysis</div>
+            <div className="row">
+              <div className="col-12 mt-1">
+                  <Doughnut data={doughnut} />
+              </div>
+            </div>
+            
         </div>
       </div>
     )
